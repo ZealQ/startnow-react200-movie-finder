8b257c1031bf04adde656935ad1fa15fdae5e20b
Zeal <mrkingtero@yahoo.com>
2018-03-01 16:37:37 -0800

    case correct

--- src/js/containers/MovieDetailContainer/index.js
+++ src/js/containers/MovieDetailContainer/index.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import MovieDetailContainer from "./movieDetailContainer"
+import MovieDetailContainer from "./moviedetailcontainer"
 
 function mapStateToProps(state) {
     return {
